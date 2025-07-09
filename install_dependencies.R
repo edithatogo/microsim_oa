0bcc48a515137d505b548615c8e4d032286a546c
edithatogo <15080672+edithatogo@users.noreply.github.com>
2025-07-10 00:17:27 +1200

    feat: Add testthat framework and initial unit test

---
+++ install_dependencies.R
@@ -0,0 +1,22 @@
+# install_dependencies.R
+
+# This script installs all the necessary packages for the AUS-OA model.
+
+# It uses the pacman package to manage packages. If pacman is not installed,
+# it will be installed first.
+
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(
+  here,
+  rmarkdown,
+  quarto,
+  readxl,
+  tidyverse,
+  logr,
+  kableExtra,
+  arrow,
+  reshape2,
+  gt,
+  testthat # for testing
+)
